b1d951b2100a6481497800e542022dc9ccdb9ecc
Teo <your.email@example.com>
2024-02-27 12:25:57 -0500

    removed extra /auth or /users for path. oops

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ import user from './user.js'
  */
 const router = express.Router();
 
-router.use('/auth', auth);
-router.use('/users', user);
+router.use('/', auth);
+router.use('/', user);
 
 export default router;
